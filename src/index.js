1a62c77358656437b1b9aa80d2bf224fa03594cd
A-iong <210410634@gms.tku.edu.tw>
2022-09-17 02:13:40 +0800

    W02-p2: Add two more books based on your ID's last digit

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import App_34 from './App_34';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <App_34 />
   </React.StrictMode>
 );
 
