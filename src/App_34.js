8c405f3ebfc049069be2f9d691229f090c41c294
A-iong <210410634@gms.tku.edu.tw>
2022-10-05 20:26:36 +0800

    W05-P3: T23_34: fetch data from Github and display them

--- src/App_34.js
+++ src/App_34.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import T21_34 from './tutorial/2-useEffect/T21_34-useEffect-basics';
+import T23_34 from './tutorial/2-useEffect/T23_34-useEffect-fetch-data';
 
 const App_34 = () => {
   return (
     <div className="container">
-      <h2>Tutorial 21 - 210410634</h2>
-      <T21_34 />
+      <h2>Tutorial 23 - 210410634</h2>
+      <T23_34 />
     </div>
   );
 };
