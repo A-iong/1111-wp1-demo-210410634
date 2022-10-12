a859027846b5c0b4afaca3fc64ac251afdccaa10
A-iong <210410634@gms.tku.edu.tw>
2022-10-12 20:38:37 +0800

    W06-P2: Show <Alert_34 /> and <List_34 />

--- src/App_34.js
+++ src/App_34.js
@@ -1,13 +1,59 @@
-import React from 'react';
-import T31_34 from './tutorial/3-conditional-rendering/T31_34_multiple-returns';
+import React, { useState, useEffect } from 'react'
+import List_34 from './components/List_34'
+import Alert_34 from './components/Alert_34'
+import { FaLuggageCart } from 'react-icons/fa';
 
 const App_34 = () => {
-  return (
-    <div className="container">
-      <h2>Tutorial 31 - 210410634</h2>
-      <T31_34 />
-    </div>
-  );
-};
+  const [name, setName] = useState('');
+  const [list, setList] = useState([]);
+  const [alert, setAlert] = useState({
+    show: false,
+    msg:'',
+    type: ''
+  });
 
-export default App_34;
+  const showAlert = (show = false, msg= '', type='',) => {
+    setAlert({show, msg, type, })
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!name){
+      showAlert(true,'please enter value','danger');
+    }else {
+      showAlert(true, 'value changed', 'success');
+      const newItem = {
+        id: new Date().getTime().toString(),
+        title: name
+      };
+      setList([...list, newItem]);
+      setName('');
+      }
+    };
+  
+
+  return (
+    <>
+      <section className="section-center">
+        <form className="grocery-from" onSubmit={handleSubmit}>
+          {alert.show && <Alert_34 alert={alert} removeAlert={showAlert}/>}
+          <h3>Grocery Bud - 210410634</h3>
+          <div className="form-control">
+            <input type="text" className="grocery" placeholder='e.g. eggs' value={name} onChange={
+              (e) => {setName(e.target.value)}}/>
+            <button type="submit" className="submit-btn" >submit</button>
+            </div>
+        </form>
+        {list.length > 0 &&(
+          <div className="grocery-container">
+            <List_34 items={list} />
+            <button className='clear-btn'>clear items</button>
+          </div>
+        )}
+      </section>
+    </>
+  
+  )
+            }
+
+export default App_34
---
+++ src/components/List_34.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import { FaEdit, FaTrash } from 'react-icons/fa'
+const List_34 = ({items}) => {
+  return (
+    <div className="grocery-list">
+      { items.map( (item) =>{
+        const {id, title } =item
+        return(
+          <article className='grocery-item' key={id}>
+            <p className='title'>{title}</p>
+            <div className="btn-container">            
+            <button className='edit-btn'>
+              <FaEdit />
+              <button type='button' className='delete-btn'>
+                <FaTrash />
+              </button>
+            </button></div>
+
+          </article>
+        )
+      })}
+    </div>
+  )
+}
+
+export default List_34
