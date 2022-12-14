a5183647df551407f8ffa020cd402e826febfc4d
A-iong <210410634@gms.tku.edu.tw>
2022-12-15 01:04:23 +0800

    end

--- src/Context_34.js
+++ src/Context_34.js
@@ -1,40 +1,49 @@
-import React, { useState, useContext, useReducer, useEffect, Children } from 'react';
+import React, {
+  useState,
+  useContext,
+  useReducer,
+  useEffect,
+  Children,
+} from 'react';
 import Reducer_34 from './Reducer_34';
 
-import cartItem from './data'
+import cartItem from './data';
 
 const AppContext_34 = React.createContext();
-
 
 const initialstate = {
   loading: false,
   cart: cartItem,
   amount: 4,
   total: 999.9,
-}
+};
 
-const AppProvider_34 = ({children}) => {
-  const [state, dispatch] =useReducer(Reducer_34,initialstate);
+const AppProvider_34 = ({ children }) => {
+  const [state, dispatch] = useReducer(Reducer_34, initialstate);
 
- useEffect(()=>{
-  dispatch({ type: 'GET_TOTALS' });
- },[state.cart]);
-
+  useEffect(() => {
+    dispatch({ type: 'GET_TOTALS' });
+  }, [state.cart]);
 
   const clearCart = () => {
-    dispatch({type: 'CLEAR_CART' });
-  }; 
+    dispatch({ type: 'CLEAR_CART' });
+  };
   const increase = (id) => {
-    dispatch({ type: 'INCREASE', payload: id })
-  }
+    dispatch({ type: 'INCREASE', payload: id });
+  };
+  const decrease = (id) => {
+    dispatch({ type: 'DECREASE', payload: id });
+  };
 
-  return  <AppContext_34.Provider value={{...state,clearCart, increase}}>
-    {children}
-  </AppContext_34.Provider>
+  return (
+    <AppContext_34.Provider value={{ ...state, clearCart, increase, decrease }}>
+      {children}
+    </AppContext_34.Provider>
+  );
 };
 
 const useGlobalContext_34 = () => {
   return useContext(AppContext_34);
-}
+};
 
-export  {AppProvider_34, useGlobalContext_34};
+export { AppProvider_34, useGlobalContext_34 };
